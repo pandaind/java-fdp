ad11db14cd8fbf697650cfc64316a6c6edb4c550
chittaranjanpanda <chittaranjan@hotmail.com>
2021-06-29 00:00:54 +0530

    setup project

---
+++ src/main/java/com/example/demo/fdp/JavaFdpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.fdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFdpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/fdp/JavaFdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.fdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
